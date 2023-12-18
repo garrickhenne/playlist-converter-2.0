d1d8bb23b64e5c7ee73c761c5698af946f4afb55
Garrick Henne <garrickhenne@outlook.com>
2023-12-18 12:47:47 -0800

    Create navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  
   return (
-    <nav>Login</nav>
+    <nav className="min-w-full p-3 flex justify-between">
+      <button onClick={() => navigate('/') }>Home</button>
+      <ul className="flex place-content-end gap-4">
+        <li><button onClick={() => navigate('login') }>Login</button></li>
+        <li><button>Register</button></li>
+      </ul>
+    </nav>
   );
 }
 
